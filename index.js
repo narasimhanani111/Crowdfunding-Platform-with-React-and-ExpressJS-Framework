423da117d051c934fc8313b1a5c45777291af818
Enugula Narasimha swamy <74957918+narasimhanani111@users.noreply.github.com>
2025-03-09 20:11:10 +0530

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  campaign: require("./campaign"),
-  user: require("./user"),
-  payment: require("./payment"),
-  donation: require("./donation"),
-  query: require("./query"),
-};
